e2f7a6d23f3124b85d3ec65e7315a2eaf5dee22c
Abu Zahrah <abuzahrah75@gmail.com>
2020-08-26 17:38:11 +0800

    include build folder

---
+++ build/precache-manifest.963006e2b3a0976552d79eb6d289002b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc725ed72ead1c2cf824a8c7fbd914b",
+    "url": "/chartjs/index.html"
+  },
+  {
+    "revision": "e8adcc358b578540c12c",
+    "url": "/chartjs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cfec928b239c0807287c",
+    "url": "/chartjs/static/js/2.843df956.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chartjs/static/js/2.843df956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8adcc358b578540c12c",
+    "url": "/chartjs/static/js/main.bfe09a23.chunk.js"
+  },
+  {
+    "revision": "f0484061747632a725a0",
+    "url": "/chartjs/static/js/runtime-main.2c5c7977.js"
+  }
+]);
