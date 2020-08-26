a850efe6ea0eb5eb5aa349ca8eeed5a91dd2ac3f
Abu Zahrah <abuzahrah75@gmail.com>
2020-08-26 17:50:52 +0800

    edit app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Cobaan Satu Dua Tiga ...</h1>
+        <h1>Cobaan Satu Dua Tiga ... Berjaya</h1>
+        <h1>thi is my first try using netlify !!!</h1>
       </header>
     </div>
   );
