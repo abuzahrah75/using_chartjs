203b1343f5659ff6d9f5515112144586e9af7ba9
Abu Zahrah <abuzahrah75@gmail.com>
2020-09-05 06:36:50 +0800

    add line & bar charts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
 
+//my charts
+import Myline from './components/newcharts/myline'
+import Mybar from './components/newcharts/mybar'
+
+
+// importing the graphs components
+// import DoughnutExample from './doughnut';
+// import DynamicDoughnutExample from './dynamic-doughnut';
+// import PieExample from './pie';
+// import LineExample from './components/line';
+import BarExample from './components/bar';
+// import HorizontalBarExample from './horizontalBar';
+// import RadarExample from './radar';
+// import PolarExample from './polar';
+// import BubbleExample from './bubble';
+// import ScatterExample from './scatter';
+// import MixedDataExample from './mix';
+// import RandomizedDataLineExample from './randomizedLine';
+// import CrazyDataLineExample from './crazyLine';
+// import LegendOptionsExample from './legend-options';
+// import LegendHandlersExample from './legend-handlers';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Cobaan Satu Dua Tiga ... Berjaya</h1>
-        <h1>thi is my first try using netlify !!!</h1>
-      </header>
+       <Myline />
+      {/* <LineExample /> */}
+      <Mybar />
     </div>
   );
 }
